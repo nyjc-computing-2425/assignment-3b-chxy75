ab05bf2f6968d62d9b84bf455c133ce98c6f505d
chxy75 <160509015+chxy75@users.noreply.github.com>
2024-03-21 15:05:06 +0000

    final

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+mantissa = stdform[:stdform.find('x')].strip(' ')
+exponent = stdform[stdform.find('^')+1 :].strip(' ')
+E = mantissa+'E'+exponent+"."
+print('This number in E notation is', E)
